337c9f18920daceb95544bd4ad53fcb612921ad0
Anton Stamenov <48581776+antonstamenov@users.noreply.github.com>
2019-03-20 12:44:33 +0000

    start new tfe job

--- infrastructure-as-code/aws-ec2-instance/variables.tf
+++ infrastructure-as-code/aws-ec2-instance/variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
   description = "AWS region"
-  default = "us-west-1"
+  default = "eu-west-1"
 }
 
 variable "ami_id" {
   description = "ID of the AMI to provision. Default is Ubuntu 14.04 Base Image"
-  default = "ami-2e1ef954"
+  default = "ami-08d658f84a6d84a80"
 }
 
 variable "instance_type" {
